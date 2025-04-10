9b4f1e0384d4851f089d58d0ae924ced063fe55a
Aakash <2024HT66518@wilp.bits-pilani.ac.in>
2025-04-10 21:11:14 +0530

    STPNew Content

--- Production/production.bat
+++ Production/production.bat
@@ -1,3 +1,3 @@
 REM Deploy files to production
-xcopy "C:\Users\Aakash\Desktop\test-aakash\Staging\*" C:\Users\Aakash\Desktop\test-aakash\Production /E /I
+robocopy "C:\Users\Aakash\Desktop\test-aakash\Staging\*" C:\Users\Aakash\Desktop\test-aakash\Production /E /I
 echo "Deployed changes to production environment."
