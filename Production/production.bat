00bf17a165f56b243abd6539492498f5007ce336
Aakash <2024HT66518@wilp.bits-pilani.ac.in>
2025-04-10 21:07:09 +0530

    STPNew Content

--- Production/production.bat
+++ Production/production.bat
@@ -1,3 +1,3 @@
 REM Deploy files to production
-xcopy C:\Users\Aakash\Desktop\test-aakash\Staging C:\Users\Aakash\Desktop\test-aakash\Production /E /I
+xcopy "C:\Users\Aakash\Desktop\test-aakash\Staging\*" C:\Users\Aakash\Desktop\test-aakash\Production /E /I
 echo "Deployed changes to production environment."
--- Staging/staging.bat
+++ Staging/staging.bat
@@ -1,3 +1,3 @@
 REM Deploy files to staging
-xcopy C:\Users\Aakash\Desktop\test-aakash C:\Users\Aakash\Desktop\test-aakash\Staging /E /I
+xcopy "C:\Users\Aakash\Desktop\test-aakash\*" C:\Users\Aakash\Desktop\test-aakash\Staging /E /I
 echo "Deployed changes to staging environment."
